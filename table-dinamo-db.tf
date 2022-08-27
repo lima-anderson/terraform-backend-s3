9c664926b7b10e3bf8901f5daf0581e83e7e0ca8
Anderson Lima <58838320+lima-anderson@users.noreply.github.com>
2022-08-27 15:35:08 -0300

    Create table-dinamo-db.tf

---
+++ table-dinamo-db.tf
@@ -0,0 +1,15 @@
+resource "aws_dynamodb_table" "dynamodb-backend" {
+  name = "dynamodb-backend"
+  hash_key = "LockID"
+  read_capacity = 1
+  write_capacity = 1
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+ 
+  tags = {
+    Name = "dynamodb-backend"
+  }
+}

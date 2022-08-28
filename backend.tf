80724100a350dc7ddd7ce32998911e80f2c099e6
Anderson Lima <58838320+lima-anderson@users.noreply.github.com>
2022-08-27 22:16:11 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,9 @@
 terraform {
-    backend "s3" {
-        profile    = "defaults"
-        bucket = "bucket-backend-s3"
-        region = "sa-east-1"
-        encrypt = "true"
-        dynamodb_table = "dynamodb-backend"
-        key = "project-name/terraform.tfstate"
-    }
+  backend "s3" {
+    bucket = "bucketbackend"
+    region = "us-east-1"
+    encrypt = true
+    key    = "projectCloudGuru/terraform.tfstate"
+    dynamodb_table = "dynamodb-backend"
+  }
 }
